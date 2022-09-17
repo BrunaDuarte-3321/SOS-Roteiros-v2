efe9abd649a4c94a9133c234debefca1b561ac58
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2022-09-17 09:27:07 -0300

    Implementando componente default  Header na aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { HeaderContainer } from './styles'
+import { HeaderContainer, NavLinkContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logosos.svg'
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <nav>
-        <a href="">header</a>
-      </nav>
+      <img src={logo} alt="" className="container" />
+      <NavLinkContainer>
+        <NavLink to="/proposta">Nossa proposta</NavLink>
+        <NavLink to="/proposta">Cadastrar/Logar</NavLink>
+      </NavLinkContainer>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 8.25rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  background: ${({ theme }) => theme.color.white};
+
+  img {
+    width: 11.19rem;
+    height: 4.5rem;
+  }
+`
+
+export const NavLinkContainer = styled.nav`
+  display: flex;
+  gap: 4.375rem;
+
+  a {
+    text-decoration: none;
+    font-weight: 400;
+    color: ${({ theme }) => theme.color['primary-green-dark']};
+    font-family: ${({ theme }) => theme.fonts.title};
+    font-size: ${({ theme }) => theme.textSizes['text-regular-large']};
+    line-height: 22px;
+    transition: 0.3s;
+
+    &:hover {
+      color: ${({ theme }) => theme.color['primary-green-light']};
+    }
+  }
+  a + a {
+    margin-right: 7.5rem;
+  }
+`
